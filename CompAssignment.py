0d138e8adf3b2faa6fd07d0e131b06e3f88424de
Aviral <the1aviral@gmail.com>
2015-11-20 23:35:35 +0530

    CompAssignment.py

--- CompAssignment.py
+++ CompAssignment.py
@@ -9,27 +9,27 @@ ip=""
 while(ip.lower()!="quit"):
  os.system("clear")
  l=random.choice(list(set(x)))
- count,u,w=4,[],list("_"*len(l))
+ count,u,w=7,[],list("_"*len(l))
  while(count>0 and "".join(w)!=l):
   print("\n",count,"attempts left\tincorrect attempts ",u,"\n")
   print("\t"," ".join(w),"\n")
   ip = input(" Enter A Charecter : ")
   if(ip.lower() in l.lower()):
-   print("Charecter ",ip," found")
+   print("Character ",ip," found")
    i=0
    while(i<len(l)):
     if(ip.lower()==l[i].lower()):
      w[i]=l[i]
     i += 1
   elif(ip.lower() not in u):
-   print("Charecter ",ip," not found")
+   print("Character ",ip," not found")
    u.append(ip)
    count -= 1
  if(count==0):
-  print("\n\nNumber Of Attempts exhausted\nThe correct word was ",l)
+  print("\n\nNumber Of Attempts exhausted\nThe correct word was :",l)
   ip=input("Enter 'quit' to quit else press any key to continue: ")
  else:
-  print("\n\nYou gussed the word with ",count," attempt(s) remaining") 
+  print("\n\nYou gussed the word ", l, "with ",count," attempt(s) remaining") 
   ip=input("Enter 'quit' to quit else press any key to continue: ") 
 source.close()  
   
