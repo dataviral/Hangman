077af45ae9233d4cab3e4f6db34c76306178528c
dataviral <the1aviral@gmail.com>
2016-03-07 14:53:20 +0530

    2nd Commit

--- Documents/java/basics/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-/* Java program for Hello World */
-
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello Everyone !!! \n This is my First program");
-	}
-}
-	  
